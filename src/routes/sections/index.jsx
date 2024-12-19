ccdcf49bec30c310152fcfba1676c8c6aeff8a17
caixaceca <07kriol@gmail.com>
2024-12-19 12:30:21 -0100

    Remove unused pages and components

--- src/routes/sections/index.jsx
+++ src/routes/sections/index.jsx
@@ -1,52 +1,26 @@
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
+import { Navigate } from 'react-router';
 
-import { MainLayout } from 'src/layouts/main';
+import { CONFIG } from 'src/global-config';
 
-import { SplashScreen } from 'src/components/loading-screen';
-
-import { authRoutes } from './auth';
 import { mainRoutes } from './main';
-import { authDemoRoutes } from './auth-demo';
 import { dashboardRoutes } from './dashboard';
-import { componentsRoutes } from './components';
 
 // ----------------------------------------------------------------------
 
-const HomePage = lazy(() => import('src/pages/home'));
 const Page404 = lazy(() => import('src/pages/error/404'));
 
 export const routesSection = [
   {
     path: '/',
-    /**
-     * @skip homepage
-     * import { Navigate } from "react-router";
-     * import { CONFIG } from 'src/global-config';
-     *
-     * element: <Navigate to={CONFIG.auth.redirectPath} replace />,
-     * and remove the element below:
-     */
-    element: (
-      <Suspense fallback={<SplashScreen />}>
-        <MainLayout>
-          <HomePage />
-        </MainLayout>
-      </Suspense>
-    ),
+    element: <Navigate to={CONFIG.auth.redirectPath} replace />,
   },
-
-  // Auth
-  ...authRoutes,
-  ...authDemoRoutes,
 
   // Dashboard
   ...dashboardRoutes,
 
   // Main
   ...mainRoutes,
-
-  // Components
-  ...componentsRoutes,
 
   // No match
   { path: '*', element: <Page404 /> },
